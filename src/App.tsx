aec8eccfe2abcd78c99e8bafc443f10f6729dbfb
Nikolay Tyurin <kolya_tyurin@mail.ru>
2020-03-28 14:15:01 +0300

    добавил макет и роутер

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,101 @@
 import React from "react";
-import { Button } from "antd";
+// import { Button } from "antd";
 import "./App.css";
 import Hello from "./components/Hello";
-const App = () => {
-  return (
-    <div className="App">
-      <Hello compiler="TypeScript" framework="React" />
-      <Button type="primary">Button</Button>
-    </div>
-  );
-};
+import Home from "./components/Home";
+import { Layout, Menu, Breadcrumb } from "antd";
+import { Route, Link } from "react-router-dom";
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined
+} from "@ant-design/icons";
 
-export default App;
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+//  
+class SiderDemo extends React.Component {
+  state = {
+    collapsed: false
+  };
+
+  onCollapse = (collapsed:boolean):void => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
+  render() {
+    return (
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
+        >
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+            <Menu.Item key="1">
+              <PieChartOutlined />
+              <Link to="/hello">Hello</Link>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <DesktopOutlined />
+              <span>Option 2</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <UserOutlined />
+                  <span>User</span>
+                </span>
+              }
+            >
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={
+                <span>
+                  <TeamOutlined />
+                  <span>Team</span>
+                </span>
+              }
+            >
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9">
+              <FileOutlined />
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: "0 16px" }}>
+            <Breadcrumb style={{ margin: "16px 0" }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360 }}
+            >
+              <Route exact path="/" component={Home}></Route>
+              <Route path="/hello" component={Hello}></Route>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            https://github.com/nik0145/turbo-giggle
+          </Footer>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+export default SiderDemo;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+
+const Nav = () =>{
+    return(
+        <div>
+            Navbar
+        </div>
+    )
+}
+
+export default Nav;
