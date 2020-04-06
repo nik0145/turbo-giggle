8a01b6d6a03ae3fd2065c02630fcd993b631eab9
Nikolay Tyurin <kolya_tyurin@mail.ru>
2020-04-06 22:53:41 +0300

    убрал formik

--- src/pages/UserLogin.tsx
+++ src/pages/UserLogin.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Formik } from "formik";
-import * as Yup from "yup";
 import { Form, Input, Button } from "antd";
 const layout = {
   labelCol: { span: 8 },
@@ -12,83 +10,40 @@ const tailLayout = {
  const onFinish = (values:any) => {
    console.log("Success:", values);
  };
+  const onFinishFailed = (errorInfo:any) => {
+    console.log("Failed:", errorInfo);
+  };
 const UserLogin = () => {
   return (
     <div>
       <p style={{ textAlign: "center" }}>Введите логин и пароль</p>
-      <Formik
-        initialValues={{
-          email: "",
-          password: "",
-        }}
-        validationSchema={Yup.object().shape({
-          email: Yup.string().email().required("Обязательно"),
-          password: Yup.string().required("Обязательно"),
-        })}
-        onSubmit={async (values) => {
-          await new Promise((resolve) => setTimeout(resolve, 500));
-          alert(JSON.stringify(values, null, 2));
-        }}
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
       >
-        {(props) => {
-          const {
-            // values,
-            // touched,
-            errors,
-            // dirty,
-            isSubmitting,
-            // handleChange,
-            // handleBlur,
-            // handleSubmit,
-            // handleReset,
-          } = props;
-          return (
-            <Form
-              {...layout}
-              name="basic"
-              onFinish={onFinish}
-              initialValues={{ remember: true }}
-              onFinishFailed={() => errors}
-            >
-              <Form.Item
-                label="Email"
-                name="email"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your email!",
-                  },
-                ]}
-              >
-                <Input />
-              </Form.Item>
-
-              <Form.Item
-                label="Password"
-                name="password"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your password!",
-                  },
-                ]}
-              >
-                <Input.Password />
-              </Form.Item>
-
-              <Form.Item {...tailLayout}>
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  disabled={isSubmitting}
-                >
-                  Submit
-                </Button>
-              </Form.Item>
-            </Form>
-          );
-        }}
-      </Formik>
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
