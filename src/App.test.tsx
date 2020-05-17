231deca324325dcffaa34576d31beb8e78bd5a02
Nikolay Tyurin <kolya_tyurin@mail.ru>
2020-05-17 19:11:23 +0300

    ее тест запустился

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import { render, act } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
+import ReactDOM from 'react-dom';
+   beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+   });
 test('app start', () => {
-  act(() => {
     // рендер компонентов
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>,div)
+    ReactDOM.unmountComponentAtNode(div)
     render(<App />);
-  });
   // const { getByText } = render(<App />);
   // console.log(getByText);
   // const linkElement = getByText(/Welcome a Home/i);
